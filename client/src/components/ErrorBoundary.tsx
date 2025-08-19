4d6807c7fe10029930e864c9522925941f7958e4
Robert Ortiz <84008858+last-orbit@users.noreply.github.com>
2025-08-19 16:12:26 +0200

    Deleted unused variables

--- client/src/components/ErrorBoundary.tsx
+++ client/src/components/ErrorBoundary.tsx
@@ -1,25 +1,27 @@
-// ErrorBoundary.tsx
-import React, { Component } from 'react';
+import { Component } from 'react';
+import type { ReactNode } from 'react';
 
-interface ErrorBoundaryState {
+type ErrorBoundaryProps = {
+  children: ReactNode;
+};
+
+type ErrorBoundaryState = {
   hasError: boolean;
-}
+};
 
-class ErrorBoundary extends Component<{}, ErrorBoundaryState> {
-  constructor(props: {}) {
+class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
+  constructor(props: ErrorBoundaryProps) {
     super(props);
     this.state = { hasError: false };
   }
 
-  static getDerivedStateFromError(error: Error) {
-    return { hasError: true };
+  componentDidCatch(error: Error) {
+    console.error(error);
   }
-
   render() {
     if (this.state.hasError) {
       return <h1>Something went wrong.</h1>;
     }
-
     return this.props.children;
   }
 }
--- client/src/pages/HomePage/HomePage.tsx
+++ client/src/pages/HomePage/HomePage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import  { useEffect, useState } from 'react';
 // import api from '../../lib/utils'; // the axios instance
 import TaskList from '../../components/TaskList';
 import ErrorBoundary from '../../components/ErrorBoundary';
